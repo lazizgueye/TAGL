3db5eb231a2197650f88d8ccc0e45c593458a7bc
gueyel <lazizgueye@gmail.com>
2015-05-03 22:27:21 +0200

    grerr

---
+++ GueyeTP1ArcheIpojo/src/main/java/com/gueye/archectypeIPOJO/GueyeTP1ArcheIpojo/SampleIntf.java
@@ -0,0 +1,9 @@
+/**
+ * iPOJO sample service interface.
+ * REMOVE THIS CLASS AS SOON AS YOU UNDERSTOOD WHAT THIS FILE GOING TO DO.
+ */
+package com.gueye.archectypeIPOJO.GueyeTP1ArcheIpojo;
+
+public interface SampleIntf {
+	String sayHello();
+}
---
+++ GueyeTP1ArcheIpojo/src/main/java/com/gueye/archectypeIPOJO/GueyeTP1ArcheIpojo/internal/SampleProviderImpl.java
@@ -0,0 +1,15 @@
+/**
+ * iPOJO sample service provider.
+ * REMOVE THIS CLASS AS SOON AS YOU UNDERSTOOD WHAT THIS FILE GOING TO DO.
+ */
+package com.gueye.archectypeIPOJO.GueyeTP1ArcheIpojo.internal;
+
+import com.gueye.archectypeIPOJO.GueyeTP1ArcheIpojo.SampleIntf;
+
+public class SampleProviderImpl implements SampleIntf {
+
+	public String sayHello() {
+		return "Hello";
+	}
+
+}
